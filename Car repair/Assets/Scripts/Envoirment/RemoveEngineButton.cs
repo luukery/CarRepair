e9341dec710d7947e372e8521da2c95755373b9a
luukery <luuk.heesbeen@Hotmail.com>
2023-03-06 16:37:25 +0100

    moving engine

--- Car repair/Assets/Scripts/Envoirment/RemoveEngineButton.cs
+++ Car repair/Assets/Scripts/Envoirment/RemoveEngineButton.cs
@@ -6,15 +6,23 @@ public class RemoveEngineButton : MonoBehaviour
 {
     [SerializeField] HoverRotation hood;
     [SerializeField] Material selectMaterial;
+    [SerializeField] GameObject engine;
+
     Vector3 startPos;
+    Vector3 engineStartPos;
+
+   Transform removedEngineLocation;
 
     bool hovering;
+    bool EngineOutSideOfCar;
     bool hasBeenPressed;
+    bool engineIsMoving;
     
 
     Renderer rend;
     Material baseMaterial;
     
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +30,13 @@ public class RemoveEngineButton : MonoBehaviour
         if (!hood)
             Debug.LogError("Hood rotation script not found");
 
+        
+
         startPos = transform.position;
+        engineStartPos = engine.transform.position;
         rend = GetComponent<Renderer>();
         baseMaterial = rend.material;
-
+        removedEngineLocation = engine.transform.GetChild(0).transform;
     }
 
     // Update is called once per frame
@@ -68,10 +79,52 @@ public class RemoveEngineButton : MonoBehaviour
         hasBeenPressed = true;        
         transform.position = new Vector3(transform.position.x, transform.position.y - 0.05f, transform.position.z);
 
-        yield return new WaitForSeconds(0.2f);
+        StartCoroutine(MoveEngineAnimation(  ));
+        yield return new WaitForEndOfFrame();
 
+        while (engineIsMoving)
+        {
+            print("waiting");
+            yield return new WaitForEndOfFrame();
+        }
+       
         transform.position = startPos;
         hasBeenPressed = false;
+
+    }
+
+    IEnumerator MoveEngineAnimation()
+    {
+        engineIsMoving = true;
+        Vector3 endlocation;
+
+        if (!EngineOutSideOfCar)
+        {
+            endlocation = removedEngineLocation.position;
+        }
+        else
+        {
+            endlocation = engineStartPos;
+        }
+
+        while (Vector3.Distance(engine.transform.position, endlocation) > 0.001f)
+        {
+            yield return null;
+
+            print(Vector3.Distance(engine.transform.position, endlocation));
+            engine.transform.position = Vector3.Slerp(engine.transform.position, endlocation, 3f * Time.deltaTime);
+
+        }
+
+        engine.transform.position = endlocation;
+
+
+        EngineOutSideOfCar = !EngineOutSideOfCar;
+
+        engineIsMoving = false;
+
+
+
 
     }
 }
