b0eb3cf547a94f30a35e289a7677e66784a57cdb
luukery <luuk.heesbeen@Hotmail.com>
2023-03-12 22:40:54 +0100

    push

---
+++ Car repair/Assets/Scripts/Envoirment/InterfaceRaycast/InterfaceRaycast.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceRaycast : MonoBehaviour
+{
+    interface raycastInteractions
+    {
+        void HoverOver();
+        void Interact();
+    }
+}
