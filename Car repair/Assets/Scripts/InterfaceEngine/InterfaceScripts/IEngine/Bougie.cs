9eb30efb3fc59cb6365cc1ac05283f1d026d575c
luukery <luuk.heesbeen@Hotmail.com>
2023-03-28 07:16:18 +0200

    xx

---
+++ Car repair/Assets/Scripts/InterfaceEngine/InterfaceScripts/IEngine/Bougie.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bougie : MonoBehaviour, IEngine
+{
+    public Canvas choiceCanvas;
+
+    public void Hover(RaycastHit hit)
+    {
+
+    }
+
+    public void Interact()
+    {
+
+    }
+}
